2023736b9f445c496e37643a9886d3cb1123d539
Oladapo <107548302+ThegreatJadefox@users.noreply.github.com>
2025-03-30 12:28:24 +0100

    Update admin.py

--- admin.py
+++ admin.py
@@ -31,7 +31,7 @@ def receive_review():
 
 def run_api():
     # Run the Flask API on a specified port (e.g., 5001)
-    app.run(port=8502, debug=True, use_reloader=False)
+    app.run(port=0)
 
 # Start the API server in a background thread
 api_thread = threading.Thread(target=run_api)
